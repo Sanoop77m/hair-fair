5acc56f1e7f921f75a07553e36a662ff5f54a1f9
sanoop <sanoop@ahedge.in>
2025-08-26 14:34:33 +0530

    updated landing page

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\sanoop\\personal\\Hair-Thim\\src\\app\\layout.tsx": [
+      "C:\\sanoop\\personal\\Hair-Thim\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"700\",\"900\"],\"display\":\"swap\"}],\"variableName\":\"roboto\"}",
+      "C:\\sanoop\\personal\\Hair-Thim\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\sanoop\\personal\\Hair-Thim\\src\\app\\page": [
+      "C:\\sanoop\\personal\\Hair-Thim\\src\\app\\globals.css",
+      "C:\\sanoop\\personal\\Hair-Thim\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"700\",\"900\"],\"display\":\"swap\"}],\"variableName\":\"roboto\"}"
+    ]
+  }
+}
